6856b251e4a0a91e8515e326f317105dcb69214f
Albeiro <albeiroespitiasierra@gmail.com>
2019-07-01 22:36:36 -0500

    Adding gatsby image plugin

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import { Link, graphql, useStaticQuery } from 'gatsby'
+import styles from './styles/blog.module.scss'
 
 
 const BlogPage = () => {
@@ -24,10 +25,10 @@ const BlogPage = () => {
 	return(
 		<Layout>
 			<h1>Blog</h1>
-			<ol>
+			<ol className={styles.posts}>
 				{data.allMarkdownRemark.edges.map((edge)=>{
 					return(
-						<li>
+						<li className={styles.post}>
 							<Link to={`/blog/${edge.node.fields.slug}`}>
 								<h2>{edge.node.frontmatter.title}</h2>
 								<p>{edge.node.frontmatter.date}</p>
